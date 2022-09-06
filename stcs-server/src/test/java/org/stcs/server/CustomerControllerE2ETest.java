2eb30ee7964b528483744088ec21d0a57b648d62
welldoer <welldoer@263.net>
2022-09-06 16:12:46 +0800

    重构测试代码：提取公共方法，同时清理无用代码。

--- stcs-server/src/test/java/org/stcs/server/CustomerControllerE2ETest.java
+++ stcs-server/src/test/java/org/stcs/server/CustomerControllerE2ETest.java
@@ -1,49 +1,30 @@
 package org.stcs.server;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONObject;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.stcs.server.entity.CustomerEntity;
 import org.stcs.server.service.CustomerService;
 
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
-//@DataMongoTest
-@SpringBootTest
-@AutoConfigureMockMvc
-//@WebMvcTest(controllers = OrderInfoController.class)
-@Slf4j
 @TestMethodOrder(MethodOrderer.MethodName.class)
 public class CustomerControllerE2ETest extends AbstractTest {
-
     private static final String CUSTOMERS_PATH = "/api/v1/customers";
-    @Autowired
-    private MockMvc mockMvc;
 
     @Autowired
     private CustomerService customerService;
 
-
     @Test
     void testAddCustomer() throws Exception {
-
         final List<CustomerEntity> customerEntities = new ArrayList<>();
         CustomerEntity customer = CustomerEntity.builder()
                 .custId(10)
@@ -51,31 +32,18 @@ public class CustomerControllerE2ETest extends AbstractTest {
                 .build();
         customerEntities.add(customer);
 
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.post(CUSTOMERS_PATH)
-                .accept(MediaType.APPLICATION_JSON).content(JSON.toJSONBytes(customerEntities))
-                .contentType(MediaType.APPLICATION_JSON);
-        MockHttpServletResponse response = mockMvc.perform(requestBuilder)
-                .andExpect(status().isOk())
-                .andReturn().getResponse();
-        log.info(response.getContentAsString());
-
+        MockHttpServletResponse response = getMockResponseByRestApiPostWithJson(CUSTOMERS_PATH, customerEntities);
         JSONObject retBodyJson = JSON.parseObject(response.getContentAsString());
 
         assertThat(retBodyJson.get("code")).isEqualTo("ok");
         assertThat(retBodyJson.get("messageId")).isNotNull();
-
     }
 
     @Test
     void testGet() throws Exception {
         String expectedResponse = "{\"custId\": 10,\"custName\": \"张三\"}";
 
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(CUSTOMERS_PATH);
-        MockHttpServletResponse response = mockMvc.perform(requestBuilder)
-                .andExpect(status().isOk())
-                .andReturn().getResponse();
-        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-        log.info(response.getContentAsString());
+        MockHttpServletResponse response = getMockResponseByRestApiGet(CUSTOMERS_PATH);
         JSONObject retBodyJson = JSONObject.parseObject(response.getContentAsString());
 
         assertThat(retBodyJson.getString("code")).isEqualTo("ok");
