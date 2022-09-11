08bc6ad48624dd89c3276009456dc28e9d644be3
welldoer <welldoer@263.net>
2022-09-11 23:39:44 +0800

    更简洁支持 Security 验证测试

--- stcs-server/src/test/java/org/stcs/server/AbstractTest.java
+++ stcs-server/src/test/java/org/stcs/server/AbstractTest.java
@@ -1,6 +1,5 @@
 package org.stcs.server;
 
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.nio.charset.StandardCharsets;
@@ -12,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -23,6 +23,7 @@ import org.testcontainers.utility.DockerImageName;
 @SpringBootTest
 @AutoConfigureMockMvc
 @Slf4j
+@WithMockUser(username = "stcs", password = "stcs")
 public class AbstractTest {
     private static final MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo"));
 
@@ -36,8 +37,7 @@ public class AbstractTest {
     }
 
     MockHttpServletResponse getMockResponseByRestApiGet(String url) throws Exception {
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(url)
-                .with(httpBasic("stcs", "stcs"));
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(url);
         MockHttpServletResponse response = mockMvc.perform(requestBuilder)
                 .andExpect(status().isOk())
                 .andReturn().getResponse();
@@ -50,7 +50,6 @@ public class AbstractTest {
 
     MockHttpServletResponse getMockResponseByRestApiPostWithJson(String url, Object jsonObject) throws Exception {
         RequestBuilder requestBuilder = MockMvcRequestBuilders.post(url)
-                .with(httpBasic("stcs", "stcs"))
                 .accept(MediaType.APPLICATION_JSON).content(JSON.toJSONBytes(jsonObject))
                 .contentType(MediaType.APPLICATION_JSON);
         MockHttpServletResponse response = mockMvc.perform(requestBuilder)
@@ -63,7 +62,6 @@ public class AbstractTest {
 
     MockHttpServletResponse getMockResponseByRestApiPutWithJson(String url, Object jsonObject) throws Exception {
         RequestBuilder requestBuilder = MockMvcRequestBuilders.put(url)
-                .with(httpBasic("stcs", "stcs"))
                 .accept(MediaType.APPLICATION_JSON).content(JSON.toJSONBytes(jsonObject))
                 .contentType(MediaType.APPLICATION_JSON);
         MockHttpServletResponse response = mockMvc.perform(requestBuilder)
@@ -75,8 +73,7 @@ public class AbstractTest {
     }
 
     MockHttpServletResponse getMockResponseByRestApiDelete(String url) throws Exception {
-        RequestBuilder requestBuilder = MockMvcRequestBuilders.delete(url)
-                .with(httpBasic("stcs", "stcs"));
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.delete(url);
         MockHttpServletResponse response = mockMvc.perform(requestBuilder)
                 .andExpect(status().isOk())
                 .andReturn().getResponse();
